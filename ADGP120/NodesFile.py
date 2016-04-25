067e5852a6eb14b97b740d9d1c658fce14f47637
bbarlowAIEYr1 <brock.barlow@students.aie.edu.au>
2016-04-25 14:50:48 -0500

    added comments

--- ADGP120/NodesFile.py
+++ ADGP120/NodesFile.py
@@ -3,18 +3,18 @@ from pygame import *
 
 class Node(object):
 	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+		self.x = x #x value
+		self.y = y #y value
 		self.f = None #g + h
 		self.g = None #movement cost
 		self.h = None #guess movement cost
-		self.parent = None
-		self.walkable = True 
-		self.color = (255,255,255)
-		self.height = 20
-		self.width = 20
-		self.margin = 5
-		self.center = (self.x + (self.width / 2), self.y + (self.height / 2))
+		self.parent = None #current node
+		self.walkable = True #if walkable or not
+		self.color = (255,255,255) #walkable path color
+		self.height = 20 #size of grid
+		self.width = 20 #size of grid
+		self.margin = 5 #space between the blocks
+		self.center = (self.x + (self.width / 2), self.y + (self.height / 2)) #used for path line
 		
 	def draw(self, screen): #this will draw the grid
 		c = self.color if (self.walkable) else (255,0,0) #walkable = white, unwalkable = red
@@ -24,7 +24,7 @@ class Node(object):
 		self.g = value
 		return self.g
 		
-	def setH(self, value): #set guess movement cost.
+	def setH(self, value): #set manhatton cost.
 		self.h = value
 		return self.h
 		
