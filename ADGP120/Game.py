4112bc04af368dab4cd150450734864b6538067f
bbarlowAIEYr1 <brock.barlow@students.aie.edu.au>
2016-04-25 15:43:57 -0500

    added comments, removed code

--- ADGP120/Game.py
+++ ADGP120/Game.py
@@ -28,7 +28,7 @@ def main():
 	for a in searchSpace:
 		for b in a:
 			randGrid = randrange(0, 10)
-			if (randGrid % 3 == 0) and (program.goal != b):
+			if (randGrid % 3 == 0) and (program.goal != b) and (program.start != b):
 				b.walkable = False
 			b.draw(screen)
 			
--- ADGP120/NodesFile.py
+++ ADGP120/NodesFile.py
@@ -1,35 +1,21 @@
-import pygame
-from pygame import *
+import pygame #using pygame
+from pygame import * #import everything
 
-class Node(object):
-	def __init__(self, x, y):
+class Node(object): #node class
+	def __init__(self, x, y): #init function
 		self.x = x #x value
 		self.y = y #y value
 		self.f = None #g + h
 		self.g = None #movement cost
-		self.h = None #guess movement cost
+		self.h = None #manhatton cost
 		self.parent = None #current node
 		self.walkable = True #if walkable or not
-		self.color = (255,255,255) #walkable path color
-		self.height = 20 #size of grid
-		self.width = 20 #size of grid
+		self.color = (255,255,255) #white color value
+		self.height = 20 #size of grid (up to down)
+		self.width = 20 #size of grid (left to right)
 		self.margin = 5 #space between the blocks
 		self.center = (self.x + (self.width / 2), self.y + (self.height / 2)) #used for path line
 		
-	def draw(self, screen): #this will draw the grid
+	def draw(self, screen): #draw function
 		c = self.color if (self.walkable) else (255,0,0) #walkable = white, unwalkable = red
-		pygame.draw.rect(screen, c, (self.x, self.y, self.width, self.height))
-		
-	def setG(self, value): #set movement cost.
-		self.g = value
-		return self.g
-		
-	def setH(self, value): #set manhatton cost.
-		self.h = value
-		return self.h
-		
-	def getF(self): #add g and h, then return that value.
-		if (self.g == None) and (self.h == None):
-			self.g = 0
-			self.h = 0
-		return self.g + self.h
+		pygame.draw.rect(screen, c, (self.x, self.y, self.width, self.height)) #used to generate grid
