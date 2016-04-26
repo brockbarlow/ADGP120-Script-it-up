1996acfa67e926a61cffbc98c7e8cca5a36e4186
brockbarlow <brock.barlow@students.aie.edu.au>
2016-04-25 23:02:16 -0500

    made edits to file

--- ADGP120/AStarFile.py
+++ ADGP120/AStarFile.py
@@ -1,6 +1,7 @@
-import pygame, NodesFile #using pygame and NodesFile
+import pygame, NodesFile, time #using pygame and NodesFile
 from pygame import * #import everything
 from NodesFile import * #import everything
+from time import *
 
 class AStar(object): #astar class
 	def __init__(self, start, searchSpace, goal): #init function
@@ -17,7 +18,7 @@ class AStar(object): #astar class
 		self.adjacentColor = (0,255,255) #teal color value
 		self.lowestF = None #what the lowest f is
 		self.hCost = 0 #what the h cost is
-		self.gCost = None #what the g cost is
+		self.gCost = 0 #what the g cost is
 		
 	def draw(self, screen): #draws start and goal nodes
 		cStart = self.startColor
@@ -27,21 +28,21 @@ class AStar(object): #astar class
 		
 	def startSetup(self, screen): 
 		self.current = self.start
-		self.current.g = 0
-		self.current.h = 0
-		self.current.g = self.getGCost(self.current, self.goal)
-		self.current.h = self.getHCost(self.current, self.goal)
+		#self.current.g = 0
+		#self.current.h = 0
+		#self.current.g = self.getGCost(self.current, self.goal)
+		#self.current.h = self.getHCost(self.current, self.goal)
 		self.OPENList.append(self.current) 
-		while self.current in self.OPENList:
-			adjacent = self.locateAdjacent(screen)
-			for a in adjacent:
-				if (a.walkable == True):
-					a.parent = self.current
-					a.g = self.getGCost(a, self.current)
-					a.h = self.getHCost(a, self.goal)
-					self.OPENList.append(a)
-			self.OPENList.remove(self.current)
-			self.CLOSEList.append(self.current)
+		#while self.current in self.OPENList:
+		adjacent = self.locateAdjacent()#screen
+		for a in adjacent:
+			if (a.walkable == True):
+				a.parent = self.current
+				a.g = self.getGCost(a, self.current)
+				a.h = self.getHCost(a, self.goal)
+				self.OPENList.append(a)
+		self.OPENList.remove(self.current)
+		self.CLOSEList.append(self.current)
 			
 	def run(self, screen):
 		self.startSetup(screen)
@@ -49,42 +50,42 @@ class AStar(object): #astar class
 			self.current = self.lowestFCost(self.OPENList)
 			self.OPENList.remove(self.current)
 			self.CLOSEList.append(self.current)
-			adjacentNode = self.locateAdjacent(screen)
+			adjacentNode = self.locateAdjacent()#screen
 			for a in adjacentNode:
 				if (a not in self.CLOSEList):
 					if (a not in self.OPENList):
-						if (self.current == self.goal):
+						if (self.goal in self.OPENList) or (self.current == self.goal):
 							return True
 						else:
-							self.OPENList.append(a)
 							a.parent = self.current
 							a.g = self.getGCost(a, self.current)
 							a.h = self.getHCost(a, self.goal)
+							self.OPENList.append(a)
 					else:
-						travelCost = self.current.g + self.current.parent.g
+						travelCost = self.current.g + self.getGCost(self.current, a)
 						if (travelCost < a.g):
 							a.parent = self.current
-							a.g = travelCost
+							a.setG(self.getGCost(self.current, a))
 							self.OPENList.sort(key = lambda x : x.f)
 		return False
 		
-	def locateAdjacent(self, screen): #function that finds current node adjacents
+	def locateAdjacent(self):#, screen): #function that finds current node adjacents
 		ADJACENTList = self.ADJACENTList
 		for a,nodes in enumerate(self.searchSpace): #get current node
 			for b,node in enumerate(nodes):
 				if node == self.current:
 					position = (b, a)
 		for a,nodes in enumerate(self.searchSpace): #get adjacent nodes
-			if (position[1] - 1 <= a <= position[1] + 1) and (a != None):
+			if (position[1] - 1 <= a <= position[1] + 1): #and (a != None):
 				for b,node in enumerate(nodes):
-					if (position[0] - 1 <= b <= position[0] + 1) and (b != None) and (self.searchSpace[a][b].walkable == True) and (self.searchSpace[a][b] != self.current):
+					if (position[0] - 1 <= b <= position[0] + 1) and (self.searchSpace[a][b].walkable == True) and (self.searchSpace[a][b] != self.current):
 						ADJACENTList.append(self.searchSpace[a][b])
-		return ADJACENTList
+		return ADJACENTList # (b != None) and
 		
 	def lowestFCost(self, nodes): #lowest f cost function. used to fine lowest f
 		lowestF = self.lowestF
 		for n in nodes: #for setting lowestF. if its none or greater than n, it becomes n
-			if (lowestF == None) or (n.f < lowestF.f):
+			if (lowestF == None) or (n.getF() < lowestF.getF()):
 				lowestF = n
 		return lowestF
 		
